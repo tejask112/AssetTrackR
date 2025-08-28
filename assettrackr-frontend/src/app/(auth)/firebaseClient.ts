424b83b2a51065a7acaaaabc06173a264d527f10
tk5g23 <tk5g23@soton.ac.uk>
2025-08-28 15:06:58 +0100

    Restructured app into (protected) and (auth) to allow for login/registration

--- assettrackr-frontend/src/app/(auth)/firebaseClient.ts
+++ assettrackr-frontend/src/app/(auth)/firebaseClient.ts
@@ -3,15 +3,10 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-
-    apiKey: "AIzaSyCTHbUKYhQ0z7VCQomGj4x8PNMLjIlk3-Y",
-    authDomain: "assettrackr-76d1d.firebaseapp.com",
-    projectId: "assettrackr-76d1d",
-    appId: "1:618780661855:web:efe3a4afc4cbeda49e63ea",
+    apiKey: process.env.firebase_apiKey,
+    authDomain: process.env.firebase_authDomain,
+    projectId: process.env.firebase_projectId,
+    appId: process.env.firebase_appId,
 };
 
 export const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
