0e25f2462f559e25bc07a309e31b815cd3cd1dda
tk5g23 <tk5g23@soton.ac.uk>
2026-02-20 19:24:18 +0000

    optimised welcome page loading

--- assettrackr-frontend/src/app/(auth)/Welcome/page.tsx
+++ assettrackr-frontend/src/app/(auth)/Welcome/page.tsx
@@ -2,7 +2,7 @@
 import Login from "./Login/Login";
 import styles from './Welcome.module.css'
 import StockTickers from "../StockTickers/StockTickers";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import Register from "./Register/Register";
 
 
@@ -23,32 +23,34 @@ export default function Welcome() {
 
     return(
         <div className={styles.entireDiv}>
-            <div className={styles.leftDiv}>
-                <StockTickers marketSector="Technology"/>
-                <StockTickers marketSector="Financials"/>
-                <StockTickers marketSector="Healthcare"/>
-                <StockTickers marketSector="Consumer Staples"/>
-                <StockTickers marketSector="Energy"/>
-            </div>
+            <Suspense>
+                <div className={styles.leftDiv}>
+                    <StockTickers marketSector="Technology"/>
+                    <StockTickers marketSector="Financials"/>
+                    <StockTickers marketSector="Healthcare"/>
+                    <StockTickers marketSector="Consumer Staples"/>
+                    <StockTickers marketSector="Energy"/>
+                </div>
             
-            <div className={styles.rightDiv}> 
-                {login && <Login/>}
-                {register && <Register/>}
+                <div className={styles.rightDiv}> 
+                    {login && <Login/>}
+                    {register && <Register/>}
 
-                {login && (
-                    <div className={styles.loginUtilitiesDiv}>
-                        <h1 className={styles.loginUtilitiesText}>Don&apos;t have an account?</h1>
-                        <button onClick={toggleRegister} className={styles.loginUtilitiesButton}>Sign Up!</button>
-                    </div>  
-                )}
+                    {login && (
+                        <div className={styles.loginUtilitiesDiv}>
+                            <h1 className={styles.loginUtilitiesText}>Don&apos;t have an account?</h1>
+                            <button onClick={toggleRegister} className={styles.loginUtilitiesButton}>Sign Up!</button>
+                        </div>  
+                    )}
 
-                {register && (
-                    <div className={styles.loginUtilitiesDiv}>
-                        <h1 className={styles.loginUtilitiesText}>Already have an account?</h1>
-                        <button onClick={toggleLogin} className={styles.loginUtilitiesButton}>Log in!</button>
-                    </div>  
-                )}
-            </div>
+                    {register && (
+                        <div className={styles.loginUtilitiesDiv}>
+                            <h1 className={styles.loginUtilitiesText}>Already have an account?</h1>
+                            <button onClick={toggleLogin} className={styles.loginUtilitiesButton}>Log in!</button>
+                        </div>  
+                    )}
+                </div>
+            </Suspense>
         </div>
     )
 }
