b57d327a5089bbee0a5cfa31f23599314a9d852e
tk5g23 <tk5g23@soton.ac.uk>
2026-03-04 22:45:56 +0000

    restructured backend

--- backend/app/db_services/supabase_client.py
+++ backend/app/db_services/supabase_client.py
@@ -1,7 +1,7 @@
 from supabase import create_client, Client
 import os
 
-url = os.getenv("PUBLIC_SUPABASE_URL", "")
-key = os.getenv("PUBLIC_SUPABASE_SERVICE_ROLE_KEY", "")
+url = os.getenv("SUPABASE_URL", "")
+key = os.getenv("SUPABASE_SERVICE_ROLE_KEY", "")
 
 supabase: Client = create_client(url, key)
